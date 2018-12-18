7e04fda99341ff2bc70fd94eb7460e0e714a3d4b
caimiao <caimiao@kingsoft.com>
2018-12-18 10:34:26 +0800

    init

---
+++ pre_txt/__init__.py
@@ -0,0 +1,13 @@
+# _*_ coding:utf-8 _*_
+'''
+@author: caimmy@qq.com
+@license: FREE
+
+@contact: http://www.github.com/caimmy
+@software: in ksg
+
+@file: __init__.py.py
+@time: 12/18/18 10:34 AM
+@desc:
+
+'''
---
+++ t1.py
@@ -0,0 +1,14 @@
+# _*_ coding:utf-8 _*_
+'''
+@author: caimmy@qq.com
+@license: FREE
+
+@contact: http://www.github.com/caimmy
+@software: in ksg
+
+@file: t1.py
+@time: 12/18/18 10:20 AM
+@desc:
+
+'''
+
